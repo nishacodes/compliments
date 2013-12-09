10949e49883f606c8053049928e1f41fa3fbf90b
nishacodes <nishabatra27@gmail.com>
2013-12-09 12:57:30 -0500

    install shotgun, create hello-world view and route, print numbers in hello-world

--- app.rb
+++ app.rb
@@ -3,8 +3,14 @@ require 'sinatra/base'
 # Why is it a good idea to wrap our App class in a module?
 module StudentSite
   class App < Sinatra::Base
-    get '/' do
-      "hello world!"
+    get '/hello-world' do
+      @random_numbers = (1..20).to_a
+      erb :hello
+    end
+
+    get '/artists' do
+      
+      erb :artist
     end
   end
 end
