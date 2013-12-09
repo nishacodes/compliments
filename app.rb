78dd9b8972ef60084b70fa05c8cb9d73272cf531
nishacodes <nishabatra27@gmail.com>
2013-12-09 15:30:08 -0500

    add lib and model

--- app.rb
+++ app.rb
@@ -1,16 +1,18 @@
 require 'sinatra/base'
+require './lib/thing.rb'
 
 # Why is it a good idea to wrap our App class in a module?
-module StudentSite
+module StudentSite # sometimes modules are used if multiple apps are made so its for namespacing
   class App < Sinatra::Base
     get '/hello-world' do
       @random_numbers = (1..20).to_a
+      @thing = Thing.new("nisha",7)
       erb :hello
     end
 
     get '/artists' do
-      
-      erb :artist
+      @artists = ["Claude Monet", "Vincent van Gogh", "Edgar Degas"]
+      erb :artists
     end
   end
 end
